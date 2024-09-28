36e304e4afe698ab0049b577430722d2950b55fd
Jan Przebor <przebot@gmail.com>
2024-09-28 21:53:07 +0200

    Change for railway

--- main.py
+++ main.py
@@ -1,48 +1,98 @@
+# A Flask app here
+
+# /new_chat(url, id)
+# /send_message(id, message)
+
+import uuid
+from flask import Flask, jsonify
+from flask import request
 from openai import OpenAI
-import random
 import json
 
-from playwright.async_api import async_playwright, TimeoutError as PlaywrightTimeoutError
-import asyncio
 from chef_client import ChefClient
+import asyncio
+import cloudinary.uploader
+import os
 
-br = False
+def create_app():
+    app = Flask(__name__)
+    state = {}
+    openai_client = OpenAI()
+    cloudinary.config( 
+        cloud_name = "dqqmqkz28", 
+        api_key = os.environ['CLOUDINARY_API_KEY'],
+        api_secret = os.environ['CLOUDINARY_API_SECRET'], # Click 'View API Keys' above to copy your API secret
+        secure=True
+    )
+    loop = asyncio.get_event_loop()
 
-url = input("Website url: ")
+    # Url and id in query
+    @app.route("/new_chat/", methods=['POST'])
+    def new_chat():
+        if 'url' not in request.args or 'id' not in request.args:
+            return "Missing url or id", 400
+        
+        url = request.args.get('url')
+        id = request.args.get('id')
 
-openai_client = OpenAI()
+        client = loop.run_until_complete(ChefClient(openai_client, url).__aenter__())
 
-async def main():
-    async with ChefClient(openai_client, url) as client:
-        while not br:
-            user_input = input("> ")
-            
-            if user_input.lower() == "exit":
-                break
+        state[id] = client
 
-            client.messages.append({"role": "user", "content": user_input})
+        return f"Chat created with url: {url} and id: {id}", 200
 
-            chat_completion = await client.query()
-            
-            client.messages.append(chat_completion.choices[0].message)
+    @app.route("/message/", methods=['POST'])
+    def send_message():
+        if 'id' not in request.args or 'message' not in request.args:
+            return "Missing id or message", 400
 
-            tool_calls = chat_completion.choices[0].message.tool_calls
-            if (tool_calls is not None):
-                for i in range(len(tool_calls)):
-                    function = tool_calls[i].function
+        id = request.args.get('id')
+        message = request.args.get('message')
 
-                    client.messages.append({
-                        "role": "tool",
-                        "content": json.dumps(await client.handle_function(function)),
-                        "tool_call_id": tool_calls[i].id
-                    })
+        if id not in state:
+            return "Chat not found", 404
+        
+        current_state = state[id]
 
-                chat_completion = await client.query()
+        current_state.messages.append({"role": "user", "content": message})
 
-                client.messages.append(chat_completion.choices[0].message)
-                print(chat_completion.choices[0].message.content)
-            else:
-                print(chat_completion.choices[0].message.content)
+        chat_completion = loop.run_until_complete(current_state.query())
+        
+        current_state.messages.append(chat_completion.choices[0].message)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+        tool_calls = chat_completion.choices[0].message.tool_calls
+        if (tool_calls is not None):
+            for i in range(len(tool_calls)):
+                function = tool_calls[i].function
+
+                current_state.messages.append({
+                    "role": "tool",
+                    "content": json.dumps(loop.run_until_complete(current_state.handle_function(function))),
+                    "tool_call_id": tool_calls[i].id
+                })
+
+            chat_completion = loop.run_until_complete(current_state.query())
+
+            current_state.messages.append(chat_completion.choices[0].message)
+            print(chat_completion.choices[0].message.content)
+        else:
+            print(chat_completion.choices[0].message.content)
+
+        loop.run_until_complete(current_state.save_screenshot())
+
+        upload_result = cloudinary.uploader.upload("screenshot.jpg", 
+            asset_folder = "pets",
+            # Random uuid 
+            public_id = f"{uuid.uuid4()}",
+            overwrite = True, 
+            resource_type = "image")
+        print(upload_result["secure_url"])
+        
+        state[id] = current_state
+        return jsonify(
+            {"response": chat_completion.choices[0].message.content, "screenshot": upload_result["secure_url"]}
+        ), 200
+    
+    return app
+
+app = create_app()
---
+++ main_input.py
@@ -0,0 +1,48 @@
+from openai import OpenAI
+import random
+import json
+
+from playwright.async_api import async_playwright, TimeoutError as PlaywrightTimeoutError
+import asyncio
+from chef_client import ChefClient
+
+br = False
+
+url = input("Website url: ")
+
+openai_client = OpenAI()
+
+async def main():
+    async with ChefClient(openai_client, url) as client:
+        while not br:
+            user_input = input("> ")
+            
+            if user_input.lower() == "exit":
+                break
+
+            client.messages.append({"role": "user", "content": user_input})
+
+            chat_completion = await client.query()
+            
+            client.messages.append(chat_completion.choices[0].message)
+
+            tool_calls = chat_completion.choices[0].message.tool_calls
+            if (tool_calls is not None):
+                for i in range(len(tool_calls)):
+                    function = tool_calls[i].function
+
+                    client.messages.append({
+                        "role": "tool",
+                        "content": json.dumps(await client.handle_function(function)),
+                        "tool_call_id": tool_calls[i].id
+                    })
+
+                chat_completion = await client.query()
+
+                client.messages.append(chat_completion.choices[0].message)
+                print(chat_completion.choices[0].message.content)
+            else:
+                print(chat_completion.choices[0].message.content)
+
+if __name__ == "__main__":
+    asyncio.run(main())
